0edbf6770ea8406a2db0c70ef30e973dacc2d5ac
Vandson <vandsonsf@gmail.com>
2021-11-09 16:48:32 -0300

    adição das funcionalidades de deleção dos eventos

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
-Route::post('/events', [EventController::class, 'store']);
+Route::post('/events', [EventController::class, 'store'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
+Route::get('/events/{id}', [EventController::class, 'edit'])->middleware('auth');
 
 Route::get('/contact', function () {
     return view('contact');
